c59acd9a458b1600d6dca9aade3747312527f5fe
David Hunt-Mateo <dhunt-mateo@tamrd.com>
2019-07-20 12:29:00 -0400

    Add getters so that Room can persist these values in the database

--- app/src/main/java/com/dmats/architectureexample/Note.java
+++ app/src/main/java/com/dmats/architectureexample/Note.java
@@ -14,4 +14,20 @@ public class Note {
     private String description;
 
     private int priority;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
 }

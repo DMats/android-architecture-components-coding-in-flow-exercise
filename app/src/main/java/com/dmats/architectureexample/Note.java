924130d1be8a27475b34f85fec6d0b468e038137
David Hunt-Mateo <dhunt-mateo@tamrd.com>
2019-07-20 12:23:48 -0400

    Add columns for id, title, description, and priority

--- app/src/main/java/com/dmats/architectureexample/Note.java
+++ app/src/main/java/com/dmats/architectureexample/Note.java
@@ -4,4 +4,12 @@ import androidx.room.Entity;
 
 @Entity(tableName = "note_table")
 public class Note {
+
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private int priority;
 }

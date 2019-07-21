27f1e05ebb9278d0d424cd6db966c23a2f85c9f8
David Hunt-Mateo <dhunt-mateo@tamrd.com>
2019-07-21 11:59:34 -0400

    Add NoteRepository constructor.
    Gets the instance of the database for the Application.
    Stores the noteDao and uses it to access the List of all Notes, wrapped in a
    LiveData so it automatically updates and is Observable.
    
    NoteDao.noteDao() is abstract, but we're able to call it because we used the
    Room.databaseBuilder() in the NoteDatabase getInstance() method so Room
    generates the implementation of noteDao() method.

--- app/src/main/java/com/dmats/architectureexample/NoteRepository.java
+++ app/src/main/java/com/dmats/architectureexample/NoteRepository.java
@@ -1,5 +1,7 @@
 package com.dmats.architectureexample;
 
+import android.app.Application;
+
 import androidx.lifecycle.LiveData;
 
 import java.util.List;
@@ -7,4 +9,10 @@ import java.util.List;
 public class NoteRepository {
     private NoteDao noteDao;
     private LiveData<List<Note>> allNotes;
+
+    public NoteRepository(Application application) {
+        NoteDatabase noteDatabase = NoteDatabase.getInstance(application);
+        noteDao = noteDatabase.noteDao();
+        allNotes = noteDao.getAllNotes();
+    }
 }

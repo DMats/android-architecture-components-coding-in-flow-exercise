95663f239cdcc1df5fd94765da28cb7ab7b6b44c
David Hunt-Mateo <dhunt-mateo@tamrd.com>
2019-07-21 10:29:10 -0400

    Add standard database operations using Room convenience annotations

--- app/src/main/java/com/dmats/architectureexample/NoteDao.java
+++ app/src/main/java/com/dmats/architectureexample/NoteDao.java
@@ -1,7 +1,19 @@
 package com.dmats.architectureexample;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
 
 @Dao
 public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
 }

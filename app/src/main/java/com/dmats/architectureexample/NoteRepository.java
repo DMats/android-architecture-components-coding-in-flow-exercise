609980680bd26a27b75e24ee541be6584ae7441c
David Hunt-Mateo <dhunt-mateo@tamrd.com>
2019-07-21 11:52:34 -0400

    Add private noteDao for accessing database operations and LiveData List of Notes

--- app/src/main/java/com/dmats/architectureexample/NoteRepository.java
+++ app/src/main/java/com/dmats/architectureexample/NoteRepository.java
@@ -1,4 +1,10 @@
 package com.dmats.architectureexample;
 
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class NoteRepository {
+    private NoteDao noteDao;
+    private LiveData<List<Note>> allNotes;
 }

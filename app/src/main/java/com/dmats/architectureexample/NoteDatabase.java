00958ea51a44fb8be2ad9de991148277220e9c93
David Hunt-Mateo <dhunt-mateo@tamrd.com>
2019-07-21 10:42:44 -0400

    Annotate NoteDatabase to add Note entity and specify version number

--- app/src/main/java/com/dmats/architectureexample/NoteDatabase.java
+++ app/src/main/java/com/dmats/architectureexample/NoteDatabase.java
@@ -1,6 +1,8 @@
 package com.dmats.architectureexample;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
 }

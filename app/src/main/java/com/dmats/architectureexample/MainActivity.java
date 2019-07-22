9d9ae72c3e1cdf3de4efe57c2187231b42531069
David Hunt-Mateo <dhunt-mateo@tamrd.com>
2019-07-22 09:05:42 -0400

    Add reference to the NoteViewModel from the Activity class

--- app/src/main/java/com/dmats/architectureexample/MainActivity.java
+++ app/src/main/java/com/dmats/architectureexample/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

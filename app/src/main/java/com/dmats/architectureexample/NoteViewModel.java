b0ecfc5ee879cbb36e0edc57ef5fd15cc02fda66
David Hunt-Mateo <dhunt-mateo@tamrd.com>
2019-07-21 23:49:58 -0400

    Add members to hold repository and List of all Notes wrapped in LiveData

--- app/src/main/java/com/dmats/architectureexample/NoteViewModel.java
+++ app/src/main/java/com/dmats/architectureexample/NoteViewModel.java
@@ -4,9 +4,17 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
 
 public class NoteViewModel extends AndroidViewModel {
+    private NoteRepository repository;
+    private LiveData<List<Note>> allNotes;
+
     public NoteViewModel(@NonNull Application application) {
         super(application);
+        repository = new NoteRepository(application);
+        allNotes = repository.getAllNotes();
     }
 }
